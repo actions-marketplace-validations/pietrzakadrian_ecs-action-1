fd28da3adae5d261e12b2d43c886e48f81413d87
Adrian Pietrzak <35579815+pietrzakadrian@users.noreply.github.com>
2023-03-07 22:42:25 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,4 @@
 #!/bin/sh -l
 set -e
 
-# Run three times to make sure we get everything
-echo $(/composer/vendor/bin/ecs check $1 --clear-cache --output-format=json > output.json)
-
-cat output.json
-OUTPUT="$(cat output.json)"
-
-OUTPUT="${OUTPUT//'%'/'%25'}"
-OUTPUT="${OUTPUT//$'\n'/'%0A'}"
-OUTPUT="${OUTPUT//$'\r'/'%0D'}"
-
-rm output.json
-echo "::set-output name=ecs_output::$OUTPUT"
+/usr/local/bin/ecs check $2 --config=ecs.php --clear-cache
